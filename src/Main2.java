48395ecba6270e0af08bbdd64cd2809a91e1f75f
alierkan704 <alierkan1313@gmail.com>
2022-12-01 13:32:23 +0300

    ödev2

---
+++ src/Main2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+double fiyat;
+Scanner inp = new Scanner(System.in);
+        System.out.println("Lütfen ürün fiyatını giriniz:");
+        fiyat = inp.nextInt();
+        double kdvOrani1=0.18;
+        double kdvOrani2=0.08;
+        double kdv1 = fiyat*kdvOrani1;
+        double kdv2 = fiyat*kdvOrani2;
+        double kdvTutari = (fiyat >= 0 && fiyat <= 1000) ? kdv1 : kdv2;
+        double sonuc=kdvTutari + fiyat;
+        double kdvOrani = (fiyat >= 0 && fiyat <= 1000) ? kdvOrani1 : kdvOrani2;
+        System.out.println("Kdv oranınız :"+kdvOrani);
+        System.out.println("Kdv fiyatınız :"+kdvTutari);
+        System.out.println("Kdv li toplam fiyatınız :"+sonuc);
+
+    }
+}
