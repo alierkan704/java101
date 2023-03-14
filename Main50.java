f7f7a23ef8cdc06436a87df8da4eb03ded979692
Ali ERKAN <92388359+alierkan704@users.noreply.github.com>
2023-03-14 20:32:59 +0300

    Update Main50.java

--- Main50.java
+++ Main50.java
@@ -5,30 +5,37 @@ import java.util.Scanner;
 public class Main50 {
     public static void main(String[] args) {
     boolean isWin=false;
+    int[] wrong = new int[5];
+    int i=0;
     int number= (int) (Math.random()*100);
     Scanner inp= new Scanner(System.in);
     System.out.println(number);
-    for(int right=1; right<=5; right++){
+    for(int right=1; right<=5; right++) {
         System.out.println("1 ile 100 arasında bir sayı giriniz:");
-        int guess=inp.nextInt();
-        if(guess==number){
-            isWin=true;
-            break;
-        }else{
-            System.out.println("Tahmin ettiğiniz sayı yanlış!");
-            System.out.println("Kalan hakkınız:"+(5-right));
-            if(guess<number){
-                System.out.println("Tahmin ettiğiniz sayı gizli sayıdan küçüktür!");
-            }else{
-                System.out.println("Tahmin ettiğiniz sayı gizli sayıdan büyüktür!");
+        int guess = inp.nextInt();
+        if (guess < 100 && guess > 1) {
+            if (guess == number) {
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Tahmin ettiğiniz sayı yanlış!");
+                wrong[i++] = guess;
+                System.out.println("Kalan hakkınız:" + (5 - right));
+                if (guess < number) {
+                    System.out.println("Tahmin ettiğiniz sayı gizli sayıdan küçüktür!");
+                } else {
+                    System.out.println("Tahmin ettiğiniz sayı gizli sayıdan büyüktür!");
+                }
             }
         }
-       }
+        System.out.println("Hatalı bir sayı girdiniz!Tekrar deneyin");
+    }
         if(isWin==true){
             System.out.println("Tebrikler tahmin ettiğiniz sayı doğru!");
         }else {
             System.out.println("Kaybettiniz!");
             System.out.println("Gizli sayı:" + number);
+            System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
         }
     }
 }
